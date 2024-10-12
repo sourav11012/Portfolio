db5a56a5fcb3e86e97f5327497be8882fa6d1cbd
Sourav Shetye <143427277+Sourav0211@users.noreply.github.com>
2024-10-12 14:27:02 -0400

    created build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
@@ -12,11 +13,17 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Helloooo");
-});
+// app.get("/", (req, res) => {
+//   res.send("Helloooo");
+// });
+
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api/v1/portfolio", require("./routes/portfolioRoutes"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT} `);
